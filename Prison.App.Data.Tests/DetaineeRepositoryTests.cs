5d4bbb4eb0e5022b050d2c2a5a7bb85f35824cba
Zinkevich <azoos@tut.by>
2018-06-09 16:42:36 +0300

    PN-1.9.2 UnitTests -Prison.App.Data,Tests project added, -Moq library added -tests for DetaineeProvider were added

---
+++ Prison.App.Data.Tests/DetaineeRepositoryTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Prison.App.Data.Tests
+{
+    [TestClass]
+    public class DetaineeRepositoryTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
